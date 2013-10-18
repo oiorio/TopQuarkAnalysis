a450bc5e2c84c3dda839751a2efe0363fe581b4e
Orso <oiorio@cern.ch>
2013-10-18 17:35:30 +0200

    added missing cfi

--- SingleTop/python/userDataMETsProducer_cfi.py
+++ SingleTop/python/userDataMETsProducer_cfi.py
@@ -7,7 +7,11 @@ topMETsPF = cms.EDProducer("SingleTopMETsProducer",
                          metsUnclDownSrc = cms.InputTag("patType1CorrectedPFMetUnclusteredEnDown"),
                          jetsSrc = cms.InputTag("selectedPatJets"),
                          muonsSrc = cms.InputTag("selectedPatMuons"),
+                         tausSrc = cms.InputTag("selectedPatTaus"),
+                         photonsSrc = cms.InputTag("selectedPatPhotons"),
+                         pfCandsNotInJetSrc = cms.InputTag("pfCandsNotInJet"),
                          isData = cms.untracked.bool(False),
+                         addExternalUnclusteredMET = cms.untracked.bool(False),
                          JESUncertaintiesPath = cms.FileInPath("TopQuarkAnalysis/SingleTop/data/Fall12_V7_DATA_UncertaintySources_AK5PFchs.txt")
 #                         JESUncertaintiesPath = cms.FileInPath("TopQuarkAnalysis/SingleTop/data/START53_V22_Uncertainty_AK5PFchs.txt")
                          )
